bd8e71443308740b18411d487b47bf375ed54a43
FishermanZzhang <1099905725@qq.com>
2018-04-14 13:23:48 +0800

    Update speed.py
    keep one graph

--- speed.py
+++ speed.py
@@ -24,29 +24,31 @@ def tf_model_speed():
         # 'nasnet_mobile', 'nasnet_large',
     ]
     for model_name in model_names:
-        model_fn = nets_factory.get_network_fn(
-            model_name,
-            num_classes=num_classes,
-            weight_decay=weight_decay,
-            is_training=is_training)
-        default_img_size = model_fn.default_image_size
-        inputs = tf.random_normal(
-            dtype=tf.float32,
-            shape=[batch_size, default_img_size, default_img_size, 3])
-        logits, _ = model_fn(
-            inputs, )
-        init = tf.global_variables_initializer()
-        with tf.Session() as sess:
-            sess.run(init)
-            start = time.time()
-            for _ in range(test_num):
-                sess.run(logits)
-            end = time.time()
-            logging.info("%s--default_image_size: %d--time: %f" %
-                         (model_name, default_img_size,
-                          1. * (end - start) / test_num))
-    for v in tf.trainable_variables():
-        logging.info(v.op.name)
+        logging.info(model_name)
+        with tf.Graph().as_default():
+            model_fn = nets_factory.get_network_fn(
+                model_name,
+                num_classes=num_classes,
+                weight_decay=weight_decay,
+                is_training=is_training)
+            default_img_size = model_fn.default_image_size
+            inputs = tf.random_normal(
+                dtype=tf.float32,
+                shape=[batch_size, default_img_size, default_img_size, 3])
+            logits, _ = model_fn(
+                inputs, )
+            # for v in tf.trainable_variables():
+            #     logging.info(v.op.name)
+            init = tf.global_variables_initializer()
+            with tf.Session() as sess:
+                sess.run(init)
+                start = time.time()
+                for _ in range(test_num):
+                    sess.run(logits)
+                end = time.time()
+                logging.info("%s--default_image_size: %d--time: %f" %
+                             (model_name, default_img_size,
+                              1. * (end - start) / test_num))
 
 
 def main(_):
